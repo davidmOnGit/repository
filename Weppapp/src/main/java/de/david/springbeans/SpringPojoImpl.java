57d15b6932214b14888ce45b0d1b94c043ef9df4
David <david.mager2@mailbox.tu-dresden.de>
2011-11-19 00:55:07 +0100

    added String property

--- Weppapp/src/main/java/de/david/springbeans/SpringPojoImpl.java
+++ Weppapp/src/main/java/de/david/springbeans/SpringPojoImpl.java
@@ -5,7 +5,16 @@ import java.util.logging.Logger;
 
 public class SpringPojoImpl implements SpringPojo{
 	
+	private String pojoString;
 	
+	public String getPojoString() {
+		return pojoString;
+	}
+
+	public void setPojoString(String pojoString) {
+		this.pojoString = pojoString;
+	}
+
 	@Override
 	public void doNothing(){
 		Logger.getLogger("SpringPojoLogger").log(Level.INFO, "Deine Springbean hat was geloggt!");
